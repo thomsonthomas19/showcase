5ad44816dd02865a4df86247170f7ea150ca307f
t2million <thomsonthomas19@gmail.com>
2019-04-04 14:39:03 -0400

    Updates

---
+++ precache-manifest.18c873e0ed1071279b7d77e787a3a7bf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2ce616c1cbbd642e17fcbdd394da459",
+    "url": "/showcase/static/media/postbank.e2ce616c.png"
+  },
+  {
+    "revision": "fdeba3cc7e17601bcab8af6c7349cd84",
+    "url": "/showcase/static/media/enyware.fdeba3cc.png"
+  },
+  {
+    "revision": "e7911fc302ce7120cca045692a4e0597",
+    "url": "/showcase/static/media/acciourl.e7911fc3.png"
+  },
+  {
+    "revision": "a7759ca4978362d26bcf",
+    "url": "/showcase/static/js/runtime~main.a7759ca4.js"
+  },
+  {
+    "revision": "8d75c1b066c4634f3398",
+    "url": "/showcase/static/js/main.8d75c1b0.chunk.js"
+  },
+  {
+    "revision": "1452296a4b0e09acaa70",
+    "url": "/showcase/static/js/1.1452296a.chunk.js"
+  },
+  {
+    "revision": "8d75c1b066c4634f3398",
+    "url": "/showcase/static/css/main.f587b51d.chunk.css"
+  },
+  {
+    "revision": "13a5aefff8028d8b5884c6509e962f96",
+    "url": "/showcase/index.html"
+  }
+];
